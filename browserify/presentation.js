155b8c528007fcba6a48622782ff16d740c3b391
Eivind Uggedal <eivind@uggedal.com>
2014-08-27 14:08:11 +0200

    browserify: modules

---
+++ browserify/presentation.js
@@ -0,0 +1,36 @@
+// JS modules
+// ----------
+
+// es6:
+import trim from './trim';
+import uppercase from './uppercase';
+
+var trimupper = function (str) {
+  return trim(uppercase(str));
+};
+export default trimupper
+
+// requirejs:
+define(['trim', 'uppercase'], function (trim, uppercase) {
+  return function (str) {
+    return trim(uppercase(str));
+  };
+});
+
+// requirejs commonjs emulation:
+define(function (require, exports, module) {
+  var trim = require('trim');
+  var uppercase = require('uppercase');
+
+  module.exports = function (str) {
+    return trim(uppercase(str));
+  };
+});
+
+// commonjs (node):
+var trim = require('./trim');
+var uppercase = require('./uppercase');
+
+module.exports = function (str) {
+  return trim(uppercase(str));
+};

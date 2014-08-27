e5d0677725140a407d4c478605f5aad5116f82ba
Eivind Uggedal <eivind@uggedal.com>
2014-08-27 13:41:21 +0200

    browserify: generate huge project

---
+++ browserify/genhuge.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+rm -r huge
+mkdir -p huge
+
+n=500
+
+for i in $(seq 1 $n); do
+  cat <<EOF > huge/$i.js
+module.exports = function (i) {
+  return i + 1;
+};
+EOF
+  yes 'filler = "abcdefghijklmnopqrstuvwxyz";' |
+    head -n $n >> huge/$i.js
+done
+
+printf "var i = 0;\n" > huge/start.js
+
+for i in $(seq 1 $n); do
+  printf "i += require('./$i')(i);\n" >> huge/start.js
+done
+
+printf "console.log(i);\n" >> huge/start.js
